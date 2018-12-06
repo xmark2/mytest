a0a2b1fedeb12f77c8cc8d422177b2af4024609b
xmark2 <xmark2@gmail.com>
2018-12-06 14:06:47 +0100

    sorting fixed

--- mytest/2_index_file_generator.py
+++ mytest/2_index_file_generator.py
@@ -1,34 +1,50 @@
 import re
 import unicodedata
 import codecs
+import locale
+
+def sort_locale(mylist):
+	# sort mylist as Hungarian alphabet
+	locale.setlocale(locale.LC_ALL, "hu_HU.utf8")
+	mylist.sort(key=locale.strxfrm)
+	return mylist
 
 def read_ascii_file(file):
-	file = codecs.open(file,'r',encoding='utf-8',errors='ignore')
+	# open file and encode as utf-8
+	file = codecs.open(file,'r',encoding='UTF-8',errors='ignore')
 	data = file.read()
+
+	# convert data rows to list
 	rows = data.replace('\r','').split('\n')
 
-	words_origin = ' '.join(rows).split(' ')
-	words = ' '.join(rows).lower().split(' ')
-	words = sorted(list(set(words)))
+	# join rows and list distinct list of words by Hungarian alphabet
+	words = ' '.join(rows)
+	words = re.findall(r'\w+',words)
+	words = sort_locale(list(set(words)))
 
+	# collect each word to my_data dictionary with their row indexes
 	my_data = {}
 	for word in words:
 		index = []
-		for j in range(0,len(rows)):
-			if word.lower() in rows[j].lower():
-				index.append(str(int(j)+1))
-				my_data.update({word:index})
+		if word.lower() not in my_data.keys():
+			for j in range(0,len(rows)):
+				if word.lower() in rows[j].lower():
+					index.append(str(int(j)+1))
+					my_data.update({word:index})
 
+	# format text output
 	text = ''
 	for key in my_data.keys():
-		text=text+key.upper()+' '+','.join(my_data[key])+'\n'
-
+		text=text+key+' '+','.join(my_data[key])+'\n'
 	return text
 
 def write_file(text, file_index):
-	file = open('szoveg-index.txt', 'w')
+	# write text to file_index
+	file = open(file_index, 'w')
 	file.write(text)
 	file.close()
+
+########################################################
 
 if __name__ == "__main__":
 	text = read_ascii_file('szoveg.txt')
